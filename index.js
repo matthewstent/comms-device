6dddf84e1a058886c7a256db1ec89ecba699f73d
matthewstent <hello@mattstent.co.uk>
2023-09-05 01:58:56 +0100

    scaffolding

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const gui_port = process.env.GUI_PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/bootstrap.min.css", function (req, res) {
+  res.sendFile(
+    __dirname + "/node_modules/bootstrap/dist/css/bootstrap.min.css"
+  );
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  let init_msg = {
+    device: process.env.DEVICE,
+    status: "Disconnected",
+    pairing: "3FGXO3",
+  };
+  io.emit("init", init_msg);
+});
+setInterval(() => {
+  io.emit("stat_msg", { log: "hello" });
+}, 2000);
+
+server.listen(gui_port, () => {
+  console.log("Device", process.env.DEVICE, "listening on *:", gui_port);
+});
