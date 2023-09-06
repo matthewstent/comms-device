d59262a5ca513c1f6c2fead37b552c955227ee9d
matthewstent <hello@mattstent.co.uk>
2023-09-06 22:54:30 +0100

    baisc handshaking

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 require("dotenv").config();
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+
 const gui_port = process.env.GUI_PORT;
+const { io } = require("socket.io-client");
 
-app.get("/bootstrap.min.css", function (req, res) {
-  res.sendFile(
-    __dirname + "/node_modules/bootstrap/dist/css/bootstrap.min.css"
-  );
+const socket = io(process.env.API_SERVER, { path: process.env.SOCKET_PATH });
+
+let device_obj = {
+  id: process.env.DEVICE,
+  pairing: process.env.PAIRING_CODE,
+  status: "disconnected",
+  auth: "A",
+};
+
+socket.on("handshake", function (data) {
+  let a = checkAuth(data);
+  if (a == true) {
+    device_obj.status = "connected";
+    socket.emit("handshake_response", device_obj);
+  }
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  let init_msg = {
-    device: process.env.DEVICE,
-    status: "Disconnected",
-    pairing: "3FGXO3",
-  };
-  io.emit("init", init_msg);
-});
-setInterval(() => {
-  io.emit("stat_msg", { log: "hello" });
-}, 2000);
-
-server.listen(gui_port, () => {
-  console.log("Device", process.env.DEVICE, "listening on *:", gui_port);
-});
+function checkAuth(data) {
+  if (data.auth == "A") {
+    return true;
+  } else {
+    return false;
+  }
+}
