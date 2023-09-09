6b253c12f8ce5e2f77eb9ffa2bacfccf7c732226
matthewstent <hello@mattstent.co.uk>
2023-09-09 02:47:04 +0100

    signalling over socket

--- index.js
+++ index.js
@@ -25,7 +25,17 @@ socket.on("gui_update", function (data) {
   console.log(data);
 });
 socket.on("establish_connection", function (data) {
-  console.log(data);
+  if (data.type == "webrtc_request_offer") {
+    console.log("RECEIVE - request_offer");
+    socket.emit("establish_connection", { type: "webrtc_return_offer" });
+    console.log("EMIT - return_offer");
+  } else if (data.type == "webrtc_provide_offer") {
+    console.log("RECEIVE - provide_offer");
+    socket.emit("establish_connection", { type: "webrtc_return_answer" });
+    console.log("EMIT - return_answer");
+  } else if (data.type == "webrtc_provide_answer") {
+    console.log("RECEIVE - provide_answer");
+  }
 });
 
 function checkAuth(data) {
