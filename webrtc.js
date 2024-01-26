3b132a26563d4c7305728f2c464e64a7e6b5c31a
matthewstent <hello@mattstent.co.uk>
2024-01-26 14:08:17 +0000

    back to spawn

--- webrtc.js
+++ webrtc.js
@@ -1,44 +1,44 @@
 let webrtc_process = "";
 import { execa } from "execa";
+import { spawn } from "child_process";
 
 export default {
   request_offer: async function (sink, source) {
     let cmdflags = ["--offer", "--source", source, "--sink", sink];
-    // return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
+      // start webrtc-cli
+      webrtc_process = spawn(
+        // "webrtc-cli --offer --source " +
+        //   1 +
+        //   " --sink " +
+        //   0 +
+        //   " --pulse-buf 60ms --source-frame 60ms --sink-frame 60ms --jitter-buf 180ms --mode lowdelay",
+        `webrtc-cli --offer --source ${source} --sink ${sink}`,
+        {
+          shell: true,
+        }
+      );
+      //   console.log("writing in to webrtc - ");
+      //   webrtc_process.stdin.write(offer);
+      //   webrtc_process.stdin.end();
 
-    // // start webrtc-cli
-    // webrtc_process = spawn(
-    //   "webrtc-cli --offer --source " +
-    //     1 +
-    //     " --sink " +
-    //     0 +
-    //     " --pulse-buf 60ms --source-frame 60ms --sink-frame 60ms --jitter-buf 180ms --mode lowdelay",
+      webrtc_process.stdout.on("data", (rdata) => {
+        let senddata = Buffer.from(rdata).toString();
+        console.log(senddata);
+        resolve(senddata);
+      });
 
-    //   {
-    //     shell: true,
-    //   }
-    // );
-    // //   console.log("writing in to webrtc - ");
-    // //   webrtc_process.stdin.write(offer);
-    // //   webrtc_process.stdin.end();
+      // console.log("doing execa");
+      // console.log(cmdflags);
+      // const { stdout } = await execa("webrtc-cli", cmdflags).stdout.pipe(s);
 
-    // webrtc_process.stdout.on("data", (rdata) => {
-    //   let senddata = Buffer.from(rdata).toString();
-    //   resolve(senddata);
-    // });
-    let s = new WritableStream();
-    console.log("doing execa");
-    console.log(cmdflags);
-    const { stdout } = await execa("webrtc-cli", cmdflags).stdout.pipe(s);
-
-    s.stdout.on("data", function (data) {
-      console.log("data start!", data, "data end!!");
+      // s.stdout.on("data", function (data) {
+      //   console.log("data start!", data, "data end!!");
+      // });
+      // console.log("done execa");
+      // console.log("???", stdout);
+      // console.log("*^^^^^*");
     });
-    console.log("done execa");
-    console.log("???", stdout);
-    console.log("*^^^^^*");
-
-    // });
   },
   request_answer: async function (offer) {
     console.log("request answer method");
