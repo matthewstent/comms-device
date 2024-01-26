d8e7ffd41f420acd6f02830190ac5294c6ac4b5e
matthewstent <hello@mattstent.co.uk>
2024-01-26 13:59:48 +0000

    typo

--- webrtc.js
+++ webrtc.js
@@ -31,7 +31,10 @@ export default {
     const { stdout } = await execa("webrtc-cli", cmdflags).stdout.pipe(
       process.stdout
     );
-    process.stdout.on("data", console.log("data start!", data, "data end!!"));
+
+    process.stdout.on("data", function (data) {
+      console.log("data start!", data, "data end!!");
+    });
     console.log("done execa");
     console.log("???", stdout);
     console.log("*^^^^^*");
